889bb70c1db4b8a32c48e54622a6cfbcb50ee220
noahhh09 <241513985+noahhh09@users.noreply.github.com>
2026-08-20 23:05:53 +0100

    Add Candidates to Cell class

--- src/core/Cell.py
+++ src/core/Cell.py
@@ -1,7 +1,8 @@
 class Cell:
-    def __init__(self, value: int | None = None, editable: bool = False):
+    def __init__(self, value: int | None = None, editable: bool = False, candidates: set[int] = set()):
         self.__value = value
         self.__editable = editable
+        self.__candidates = candidates
 
     def setValue(self, value: int | None):
         if not self.__editable:
@@ -21,3 +22,24 @@ class Cell:
 
     def isEditable(self) -> bool:
         return self.__editable
+
+    def addCandidate(self, value: int):
+        if not self.__editable:
+            raise Exception("Cell is not editable.")
+
+        if not 1 <= value <= 9:
+            raise ValueError("Value must be between 1 and 9 inclusive.")
+
+        self.__candidates.add(value)
+
+    def removeCandidate(self, value: int):
+        if not self.__editable:
+            raise Exception("Cell is not editable.")
+
+        if not value in self.__candidates:
+            raise ValueError(f"Candidate {value} not present.")
+
+        self.__candidates.remove(value)
+
+    def getCandidates(self) -> set[int]:
+        return self.__candidates
