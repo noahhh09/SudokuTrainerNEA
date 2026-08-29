6bdd2cb6a61cae17bc4fa316dc347ed0ed7bfc4f
noahhh09 <241513985+noahhh09@users.noreply.github.com>
2026-08-29 16:50:17 +0100

    Refactor 'wasAdded' in candidate moves to 'add' to be more explicit

--- src/core/Move.py
+++ src/core/Move.py
@@ -45,16 +45,16 @@ class ValueChangeMove(Move):
         return f"ValueChangeMove({self.row}, {self.col}, {self.oldValue}, {self.newValue})"
 
 class CandidateChangeMove(Move):
-    def __init__(self, row: int, col: int, candidateNumber: int, wasAdded: bool) -> None:
+    def __init__(self, row: int, col: int, candidateNumber: int, add: bool) -> None:
         super().__init__(row, col)
         self.candidateNumber = candidateNumber
-        self.wasAdded = wasAdded
+        self.add = add
 
     def apply(self, board: BoardState):
         cell = board.getCell(self.row, self.col)
 
         if cell.isEditable() and cell.getValue() is None:
-            if self.wasAdded:
+            if self.add:
                 cell.addCandidate(self.candidateNumber)
             else:
                 cell.removeCandidate(self.candidateNumber)
@@ -63,25 +63,25 @@ class CandidateChangeMove(Move):
         cell = board.getCell(self.row, self.col)
         
         if cell.isEditable() and cell.getValue() is None:
-            if self.wasAdded:
+            if self.add:
                 cell.removeCandidate(self.candidateNumber)
             else:
                 cell.addCandidate(self.candidateNumber)
 
     def __str__(self) -> str:
-        return f"CandidateChangeMove({self.row}, {self.col}, {self.candidateNumber}, {self.wasAdded})"
+        return f"CandidateChangeMove({self.row}, {self.col}, {self.candidateNumber}, {self.add})"
 
 class EliminationChangeMove(Move):
-    def __init__(self, row: int, col: int, candidateNumber: int, wasAdded: bool) -> None:
+    def __init__(self, row: int, col: int, candidateNumber: int, add: bool) -> None:
         super().__init__(row, col)
         self.candidateNumber = candidateNumber
-        self.wasAdded = wasAdded
+        self.add = add
 
     def apply(self, board: BoardState):
         cell = board.getCell(self.row, self.col)
 
         if cell.isEditable() and cell.getValue() is None:
-            if self.wasAdded:
+            if self.add:
                 cell.eliminateCandidate(self.candidateNumber)
             else:
                 cell.uneliminateCandidate(self.candidateNumber)
@@ -90,10 +90,10 @@ class EliminationChangeMove(Move):
         cell = board.getCell(self.row, self.col)
         
         if cell.isEditable() and cell.getValue() is None:
-            if self.wasAdded:
+            if self.add:
                 cell.uneliminateCandidate(self.candidateNumber)
             else:
                 cell.eliminateCandidate(self.candidateNumber)
 
     def __str__(self) -> str:
-        return f"EliminationChangeMove({self.row}, {self.col}, {self.candidateNumber}, {self.wasAdded})"
+        return f"EliminationChangeMove({self.row}, {self.col}, {self.candidateNumber}, {self.add})"
