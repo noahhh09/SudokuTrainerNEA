495b2d9d4a7b9833e17d10dc195acd23d46e35cb
noahhh09 <241513985+noahhh09@users.noreply.github.com>
2026-09-20 12:14:16 +0100

    Optimise candidate population

--- src/analysis/BoardUtils.py
+++ src/analysis/BoardUtils.py
@@ -10,42 +10,39 @@ def _copyAndPopulateCandidates(board: BoardState) -> BoardState:
     # 2. Filter down the list of all possible values (1 through 9) from each row/col/block, based on its contents. Essentially NOTing 1-9 with the contents above
     # 3. For each cell, find the INTERSECTION between the respective row, col, and block's available candidates for that cell.
     
-    rowContents: list[list[int]] = [[] for _ in range(9)]
-    colContents: list[list[int]] = [[] for _ in range(9)]
-    blkContents: list[list[int]] = [[] for _ in range(9)]
+    rowContents: list[set[int]] = [set() for _ in range(9)]
+    colContents: list[set[int]] = [set() for _ in range(9)]
+    blkContents: list[set[int]] = [set() for _ in range(9)]
 
     # Forms the row, column, and block arrays to more easily cross-check each row/col/block.
     for i in range(9):
         for j in range(9):
             value = board.getCell(i, j).getValue()
 
-            if value != None:
-                rowContents[i].append(value)
-                colContents[j].append(value)
+            if value is not None:
+                rowContents[i].add(value)
+                colContents[j].add(value)
 
                 blkIndex = board.getBlockIndex(i, j)
-                blkContents[blkIndex].append(value)
+                blkContents[blkIndex].add(value)
 
-    possibleValues = [1,2,3,4,5,6,7,8,9]
+    possibleValues = {1,2,3,4,5,6,7,8,9}
 
     # Sets the available values in each row/col/block to all values NOT already in that row/col/block.
-    rowAvailables = [[value for value in possibleValues if value not in row] for row in rowContents]
-    colAvailables = [[value for value in possibleValues if value not in col] for col in colContents]
-    blkAvailables = [[value for value in possibleValues if value not in blk] for blk in blkContents]
+    rowAvailables = [possibleValues - row for row in rowContents]
+    colAvailables = [possibleValues - col for col in colContents]
+    blkAvailables = [possibleValues - blk for blk in blkContents]
 
     # Merges all row/col/block availables into one 9x9 "board-state-like" array representing all available digits in each cell in a given board state.
     # (essentially: rowAvailables ∩ colAvailables ∩ blkAvailables)
     for i in range(9):
         for j in range(9):
             if board.getCell(i, j).getValue() is not None:
-                continue
+                continue # We are only concerned about adding candidates to empty cells
 
             blockIdx = board.getBlockIndex(i, j)
 
-            intersection = {
-                value for value in rowAvailables[i] 
-                if value in colAvailables[j] and value in blkAvailables[blockIdx]
-            }
+            intersection = rowAvailables[i] & colAvailables[j] & blkAvailables[blockIdx]
 
             board.getCell(i, j).addCandidates(intersection)
 
