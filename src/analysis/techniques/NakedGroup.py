75395c00289c7d64eff9144d2c41881b7077ae7c
noahhh09 <241513985+noahhh09@users.noreply.github.com>
2026-09-20 14:18:45 +0100

    (TS) Generalise Naked groups using @classmethod

--- src/analysis/techniques/NakedGroup.py
+++ src/analysis/techniques/NakedGroup.py
@@ -8,66 +8,10 @@ from src.core.Cell import Cell
 from src.core.Move import EliminationChangeMove, Move
 
 
-class NakedPair(Technique):
-    displayName = "Naked Pair"
-    description = "A technique where two cells exclusively have the same two candidates remaining."
-
-    def __init__(self, moves: list[Move], unit: Unit, causalPositions: set[int], candidateUnion: set[int]):
-        super().__init__(moves)
-        self.unit = unit
-        self.causalPositions = causalPositions
-        self.candidateUnion = candidateUnion
-
-    def identity(self) -> str:
-        return f"{self.__class__.__name__};{self.unit};{self.causalPositions};{self.candidateUnion}"
-
-    def getHintData(self) -> dict[str, str]:
-        unit = self.unit
-
-        return {
-            "Unit": f"{unit.unitType.name} {unit.unitIndex + 1}",
-            "Causal Positions": " ".join(f"{pos[0], pos[1]}" for pos in (unit.getBoardPosition(relPos) for relPos in self.causalPositions)),
-            "Digits": ", ".join(str(digit) for digit in sorted(list(self.candidateUnion)))
-        }
-
-    @classmethod
-    def findAvailable(cls, state: BoardState) -> list[Technique]:
-        candidatesState = BoardUtils.copyAndPopulateCandidates(state)
-        units = BoardUtils.getAllUnits(candidatesState)
-
-        found: list[Technique] = []
-
-        for unit in units:
-            cells = list(enumerate(unit.cells))
-            nakedPairs = _findNakedGroups(cells, 2)
-
-            for positions, union in nakedPairs:
-                moves: list[Move] = []
-
-                positionsToCheck = set(range(9)) - set(positions)
-            
-                for relPos in positionsToCheck:
-                    candidatesToEliminate = unit.cells[relPos].getEffectiveCandidates().intersection(union)
-    
-                    if candidatesToEliminate:
-                        row, col = unit.getBoardPosition(relPos)
-                        for n in candidatesToEliminate:
-                            elim = EliminationChangeMove(row, col, n, True)
-                            moves.append(elim)
-
-                if len(moves) != 0:
-                    tech = NakedPair(moves, unit, set(positions), union)
-                    found.append(tech)
-
-        return found
-
-    # Used for debugging.
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}({self.unit.unitType.name} {self.unit.unitIndex} POSITIONS{self.causalPositions} UNION{self.candidateUnion}, {self.moves})"
-
-class NakedTriple(Technique):
-    displayName = "Naked Triple"
-    description = "A technique where three cells contain only three mutual candidates between them, allowing candidates to be ruled out in other cells."
+class NakedGroup(Technique):
+    displayName = "Naked Group"
+    description = "A technique where n cells contain only n mutual candidates between them, allowing candidates to be ruled out in other cells."
+    degree: int
 
     def __init__(self, moves: list[Move], unit: Unit, causalPositions: set[int], candidateUnion: set[int]):
         super().__init__(moves)
@@ -98,8 +42,8 @@ class NakedTriple(Technique):
             # Iterate through all cell's candidates. If they match, then all other instances of those candidates can be removed from other cells.
             cells = list(enumerate(unit.cells))
 
-            nakedTriples = _findNakedGroups(cells, 3)
-            for positions, union in nakedTriples:
+            nakedGroups = _findNakedGroups(cells, cls.degree)
+            for positions, union in nakedGroups:
                 moves: list[Move] = []
 
                 positionsToCheck = set(range(9)) - set(positions)
@@ -113,8 +57,8 @@ class NakedTriple(Technique):
                             elim = EliminationChangeMove(row, col, n, True)
                             moves.append(elim)
 
-                if len(moves) != 0:
-                    tech = NakedTriple(moves, unit, set(positions), union)
+                if len(moves) > 0:
+                    tech = cls(moves, unit, set(positions), union)
                     found.append(tech)
 
         return found
@@ -122,6 +66,16 @@ class NakedTriple(Technique):
     # Used for debugging.
     def __str__(self) -> str:
         return f"{self.__class__.__name__}({self.unit.unitType.name} {self.unit.unitIndex} POSITIONS{self.causalPositions} UNION{self.candidateUnion}, {self.moves})"
+
+class NakedPair(NakedGroup):
+    displayName = "Naked Pair"
+    description = "A technique where two cells exclusively have the same two candidates remaining."
+    degree = 2
+
+class NakedTriple(NakedGroup):
+    displayName = "Naked Triple"
+    description = "A technique where 3 cells contain only 3 mutual candidates between them, allowing candidates to be ruled out in other cells."
+    degree = 3
 
 # Returns [(relPositions, candidateValues)]
 def _findNakedGroups(cells: list[tuple[int, Cell]], size: int) -> list[tuple[list[int], set[int]]]:
